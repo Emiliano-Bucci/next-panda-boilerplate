d41fd2d5fa9d6cb580f9667585f2efd4fc493d2b
Emiliano-Bucci <emiliano.bucci@outlook.com>
2024-06-12 11:00:41 +0200

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'next',
+  ],
+  env: { browser: true, node: true, es6: true },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'react/display-name': 'off',
+    '@next/next/no-img-element': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
---
+++ panda.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  minify: true,
+  preflight: true,
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  exclude: [],
+  cssVarRoot: ":root",
+  outdir: "src/styled-system",
+  theme: {
+    extend: {},
+  },
+});
