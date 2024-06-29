4e853553e5f1ca3240450498bf0fac158c05e651
Emiliano-Bucci <emiliano.bucci@outlook.com>
2024-06-29 12:46:58 +0200

    feat: simplify postcss.config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,3 @@
 module.exports = {
-  plugins: {
-    "@pandacss/dev/postcss": {},
-    "@csstools/postcss-cascade-layers": {},
-    "postcss-flexbugs-fixes": {},
-    "postcss-preset-env": {
-      autoprefixer: {
-        flexbox: "no-2009",
-      },
-      stage: 2,
-      features: {
-        "custom-properties": true,
-      },
-    },
-  },
+  plugins: ["@pandacss/dev/postcss", "postcss-preset-env"],
 };
