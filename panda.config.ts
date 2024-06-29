b73cf3c187432be7708b5faffa215d2c71028780
Emiliano-Bucci <emiliano.bucci@outlook.com>
2024-06-29 11:40:36 +0200

    feat: add basic breakpoints to panda config

--- panda.config.ts
+++ panda.config.ts
@@ -9,6 +9,17 @@ export default defineConfig({
   outdir: "src/styled-system",
   jsxFramework: "react",
   theme: {
-    extend: {},
+    extend: {
+      breakpoints: {
+        base: "0px",
+        xxs: "340px",
+        xs: "600px",
+        md: "768px",
+        lg: "1024px",
+        lgx: "1280px",
+        lgxx: "1440px",
+        xl: "1600px",
+      },
+    },
   },
 });
